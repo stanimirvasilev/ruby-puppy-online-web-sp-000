ab2a99661b0217e155830868a513d6e8e1cd3a20
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-17 09:56:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,41 @@
 # Add your code here
+require "pry"
+ class Dog
+   
+   @@all = []
+   
+   def initialize(name)
+     @name = name
+   save
+   end
+   
+   def name
+     @name
+   end
+   
+   def save
+        @@all << self
+   end
+   
+   def self.all
+     @@all
+   end
+   
+   def self.clear_all
+     @@all.clear
+     
+   end
+  
+   
+   def self.print_all
+     #binding.pry
+     self.all.map do |dog| 
+       puts dog.name
+     end
+   
+   end
+  
+    
+   
+   
+  end
